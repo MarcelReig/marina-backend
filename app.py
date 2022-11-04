4f0ea168e39ddc7825fb5ec758d00187f160fc07
Marcel Reig <marcelreig@gmail.com>
2022-11-04 18:10:54 +0100

    add authentication

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import (
     Flask,
     request,
@@ -13,42 +14,82 @@ from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
 from flask_cors import CORS
-from config import config
-
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
+from config import DevelopmentConfig
 
+load_dotenv()
+
+# Create Flask app
 app = Flask(__name__)
 CORS(app)
+app.config.from_object(DevelopmentConfig)
+
+
+# secret key para la session
+app.secret_key = app.config['SECRET_KEY']
+
+
+# jwt
+JWT_KEY = app.config['JWT_KEY']
+jwt = JWTManager(app)
+
 
 # Atlas connection
-cluster = MongoClient(
-    "mongodb+srv://MarcelReig:Ml00TSOSq4CBTsMN@cluster0.5mwmz.mongodb.net/marina-back-end?retryWrites=true&w=majority"
-)
+CONNECTION_STRING = app.config['MONGO_CLUSTER']
+
+cluster = MongoClient(CONNECTION_STRING)
 
 mongo = cluster["marina_db"]
 
-# secret key para la session
-app.secret_key = "6+8zZ69dzChLZCU9h=XE+Gren}fnRV"
 
-
-######################
-####    Routes
-#####################
+# ---------------------------------------------------------------------------- #
+#    Routes
+# ---------------------------------------------------------------------------- #
 @app.route("/")
 def _app():
     return render_template("login.html")
 
 
-######################
-####    Login
-#####################
+# ---------------------------------------------------------------------------- #
+#    Login from react
+# ---------------------------------------------------------------------------- #
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    if email != "test@test.com" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token": access_token}
+    return response
+
+
+@app.after_request
+def _after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "https://king-prawn-app-dr5rk.ondigitalocean.app/"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, " \
+                                                       "X-CSRF-Token, Authorization "
+    return response
+
+
+# ---------------------------------------------------------------------------- #
+#    Login from flask
+# ---------------------------------------------------------------------------- #
 @app.route("/login", methods=["GET", "POST"])
 def _login():
-
     if request.method == "POST":
         session["user"] = request.form["email"]
         session["password"] = request.form["password"]
-        return redirect(url_for("portfolioManager"))
+        return redirect(url_for("portfolio_manager"))
     else:
         return "bad request"
 
@@ -56,14 +97,14 @@ def _login():
 @app.route("/logout")
 def _logout():
     session.clear()
-    return redirect(url_for("app"))
+    return redirect(url_for("_app"))
 
 
-#######################################
-####    If Login redirect to manager
-######################################
+# ---------------------------------------------------------------------------- #
+#    If Login redirect to manager
+# ---------------------------------------------------------------------------- #
 @app.route("/manager")
-def portfolioManager():
+def portfolio_manager():
     # Comparar el email y la contraseña del formulario con la base de datos
 
     if session["user"] == "marcel@ibm.com" and session["password"] == "abcd1234":
@@ -73,11 +114,11 @@ def portfolioManager():
         return "You do not have permission to view this"
 
 
-#######################################
-####    Create users
-######################################
+# ---------------------------------------------------------------------------- #
+#    Create users
+# ---------------------------------------------------------------------------- #
 @app.route("/users", methods=["POST"])
-def createUser():
+def create_user():
     username = request.form["username"]
     email = request.form["email"]
     password = request.form["password"]
@@ -91,48 +132,47 @@ def createUser():
             {"_id": str(id), "username": username, "password": password, "email": email}
         )
         response.status_code = 201
-        return redirect(url_for("portfolioManager"))
+        return redirect(url_for("portfolio_manager"))
     else:
         return "not_found"
 
 
-#######################################
-####    Get all users
-######################################
+# ---------------------------------------------------------------------------- #
+#    Get all users
+# ---------------------------------------------------------------------------- #
 @app.route("/users", methods=["GET"])
-def getUsers():
+def get_users():
     users = mongo.users.find()
     response = json_util.dumps(users)
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Get one user
-######################################
+# ---------------------------------------------------------------------------- #
+#    Get one user
+# ---------------------------------------------------------------------------- #
 @app.route("/users/<id>", methods=["GET"])
-def getUser(id):
+def get_one_user(id):
     user = mongo.users.find_one({"_id": ObjectId(id)})
     response = json_util.dumps(user)
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Delete one user
-######################################
+# ---------------------------------------------------------------------------- #
+#    Delete one user
+# ---------------------------------------------------------------------------- #
 @app.route("/delete/<id>", methods=["DELETE", "GET"])
 def delete(id):
     mongo.users.delete_one({"_id": ObjectId(id)})
     response = jsonify({"message": "User" + id + " Deleted Successfully"})
     response.status_code = 200
-    return redirect(url_for("portfolioManager"))
+    return redirect(url_for("portfolio_manager"))
 
 
-#######################################
-####    Update one user
-######################################
+# ---------------------------------------------------------------------------- #
+#    Update one user
+# ---------------------------------------------------------------------------- #
 @app.route("/update/<id>", methods=["POST", "GET"])
 def update(id):
-
     user = mongo.users.find_one({"_id": ObjectId(id)})
 
     if request.method == "POST":
@@ -146,23 +186,23 @@ def update(id):
                 }
             },
         )
-        newPassword = request.form["password"]
-        hashed_password = generate_password_hash(newPassword)
+        new_password = request.form["password"]
+        hashed_password = generate_password_hash(new_password)
         mongo.users.update_one(
             {"_id": ObjectId(id)}, {"$set": {"password": hashed_password}}
         )
 
-        return redirect(url_for("portfolioManager"))
+        return redirect(url_for("portfolio_manager"))
 
     # Redirect to update one user form
     return render_template("update.html", user=user)
 
 
-#######################################
-####    Add Porfolio items
-######################################
+# ---------------------------------------------------------------------------- #
+#    Add Porfolio items
+# ---------------------------------------------------------------------------- #
 @app.route("/add", methods=["POST"])
-def addPortfolioItem():
+def add_portfolio_item():
     # Getting Input from (Postman) in JSON format
     jsonvalue = request.json
     # Picking the data from Variable
@@ -196,42 +236,42 @@ def addPortfolioItem():
         return "not_found"
 
 
-#######################################
-####    Get all portfolio items
-######################################
+# ---------------------------------------------------------------------------- #
+#    Get all portfolio items
+# ---------------------------------------------------------------------------- #
 @app.route("/portfolio", methods=["GET"])
-def getPortfolioItems():
-    portfolioItems = mongo.portfolio_items.find()
-    response = json_util.dumps(portfolioItems)
+def get_portfolio_items():
+    portfolio_items = mongo.portfolio_items.find()
+    response = json_util.dumps(portfolio_items)
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Get one portfolio item
-######################################
+# ---------------------------------------------------------------------------- #
+#    Get one portfolio item
+# ---------------------------------------------------------------------------- #
 @app.route("/portfolio/<id>", methods=["GET"])
-def getPortfolioItem(id):
-    portfolioItem = mongo.portfolio_items.find_one({"_id": ObjectId(id)})
-    response = json_util.dumps(portfolioItem)
+def get_portfolio_item(id):
+    portfolio_item = mongo.portfolio_items.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(portfolio_item)
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Delete one portfolio item
-######################################
+# ---------------------------------------------------------------------------- #
+#    Delete one portfolio item
+# ---------------------------------------------------------------------------- #
 @app.route("/portfolio/<id>", methods=["DELETE"])
-def deletePfolioItem(id):
+def delete_portfolio_item(id):
     mongo.portfolio_items.delete_one({"_id": ObjectId(id)})
     response = jsonify({"message": "Portfolio item" + id + " Deleted Successfully"})
     response.status_code = 200
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Add items to store
-######################################
+# ---------------------------------------------------------------------------- #
+#   Add items to store
+# ---------------------------------------------------------------------------- #
 @app.route("/store", methods=["POST"])
-def addStoreItem():
+def add_store_item():
     # Getting Input from (Postman) in JSON format
     jsonvalue = request.json
     # Picking the data from Variable
@@ -264,39 +304,41 @@ def addStoreItem():
     else:
         return "not_found"
 
-#######################################
-####    Get all store items
-######################################
+
+# ---------------------------------------------------------------------------- #
+#    Get all store items
+# ---------------------------------------------------------------------------- #
 @app.route("/store", methods=["GET"])
-def getStoreItems():
-    storeItems = mongo.store_items.find()
-    response = json_util.dumps(storeItems)
+def get_store_items():
+    store_items = mongo.store_items.find()
+    response = json_util.dumps(store_items)
     return Response(response, mimetype="application/json")
 
-#######################################
-####    Get one store item
-######################################
+
+# ---------------------------------------------------------------------------- #
+#    Get one store item
+# ---------------------------------------------------------------------------- #
 @app.route("/store/<id>", methods=["GET"])
-def getStoreItem(id):
-    storeItem = mongo.store_items.find_one({"_id": ObjectId(id)})
-    response = json_util.dumps(storeItem)
+def get_one_store_item(id):
+    store_item = mongo.store_items.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(store_item)
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####    Delete one store item
-######################################
+# ---------------------------------------------------------------------------- #
+#    Delete one store item
+# ---------------------------------------------------------------------------- #
 @app.route("/store/<id>", methods=["DELETE"])
-def deleteStoreItem(id):
+def delete_one_store_item(id):
     mongo.store_items.delete_one({"_id": ObjectId(id)})
     response = jsonify({"message": "Store item" + id + " Deleted Successfully"})
     response.status_code = 200
     return Response(response, mimetype="application/json")
 
 
-#######################################
-####
-######################################
+# ---------------------------------------------------------------------------- #
+#
+# ---------------------------------------------------------------------------- #
 @app.errorhandler(404)
 def not_found(error=None):
     message = {"message": "Resource Not Found " + request.url, "status": 404}
@@ -306,5 +348,4 @@ def not_found(error=None):
 
 
 if __name__ == "__main__":
-    app.config.from_object(config["development"])
     app.run()
--- config.py
+++ config.py
@@ -1,11 +1,30 @@
-class Config:
-    SECRET_KEY = 'ClaveSecreta!'
-    
+# settings.py
+import os
+from os import environ, path
+from dotenv import load_dotenv
+
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "6+8zZ69dzChLZCU9h=XE+Gren}fnRV"
+    MONGO_CLUSTER = os.getenv("ATLAS_URI")
+    JWT_KEY = os.getenv("JWT_SECRET")
+
 
 class DevelopmentConfig(Config):
-    DEBUG=True
+    DEVELOPMENT = True
+    DEBUG = True
 
 
-config={
-  'development':DevelopmentConfig
-}
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+
+
+
