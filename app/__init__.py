4ec4ed49308acdd497e5a35c6731a1c2e34642d6
Marcel Reig <marcelreig@gmail.com>
2025-09-22 23:29:19 +0200

    edit CORS

--- app/__init__.py
+++ app/__init__.py
@@ -57,9 +57,7 @@ def init_extensions(app):
     # CORS
     CORS(app, supports_credentials=True, origins=[
         "http://localhost:5173", 
-        "http://localhost:5174",
-        "https://king-prawn-app-dr5rk.ondigitalocean.app",
-        "https://marina-ilustracion.netlify.app"
+        "https://marina-ibarra.netlify.app/",
     ])
     
     # JWT
